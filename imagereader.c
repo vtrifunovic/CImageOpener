15c10ad6317f2ca57b85136acc1df150510ecf3d
vtrifunovic <76224191+vtrifunovic@users.noreply.github.com>
2022-01-24 20:19:18 -0700

    Add files via upload

--- imagereader.c
+++ imagereader.c
@@ -10,21 +10,21 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
-#define checkImageWidth 1920
-#define checkImageHeight 1080
+#define checkImageWidth 640
+#define checkImageHeight 800
 GLubyte checkImage[checkImageHeight][checkImageWidth][3];
+GLubyte checkImage_copy[checkImageHeight][checkImageWidth][3];
 
 void run_Shift()
 {
-	//printf("%d, %d\n", width, height);
 	
 	for (int x = 0; x < checkImageHeight; x++)
 	{
 		for (int y = 0; y < checkImageWidth; y++)
 		{
-			checkImage[x][y][0] += (GLubyte) 10;
-			checkImage[x][y][1] += (GLubyte) 20;
-			checkImage[x][y][2] += (GLubyte) 30;
+			checkImage[x][y][0] += 10;
+			checkImage[x][y][1] += 20;
+			checkImage[x][y][2] += 30;
 		}
 	}
 	glDrawPixels(checkImageWidth, checkImageHeight, GL_RGB, GL_UNSIGNED_BYTE, checkImage);
@@ -32,8 +32,7 @@ void run_Shift()
 }
 
 void run_Split()
-{
-	
+{	
 	for (int i = 0; i+5 < checkImageHeight-5; i++)
 	{
 		for (int j = 0; j+5 < checkImageWidth-5; j++)
@@ -49,66 +48,485 @@ void run_Split()
 		{
 			checkImage[i][j][1] = (GLubyte) checkImage[i-3][j-1][1];
 		}
-	}
+	}	
 	glDrawPixels(checkImageWidth, checkImageHeight, GL_RGB, GL_UNSIGNED_BYTE, checkImage);
 	glFlush();
 }
 
-void dilate()
+void make_grayscale()
 {
 	for (int x = 0; x < checkImageHeight; x++)
 	{
 		for (int y = 0; y < checkImageWidth; y++)
 		{
-			checkImage[x][y][0] += (GLubyte) 3;
-			if (x+1 < checkImageHeight)
+			float c = (checkImage[x][y][0] + checkImage[x][y][1] + checkImage[x][y][2])/3;
+			checkImage[x][y][0] = (int)c;
+			checkImage[x][y][1] = (int)c;
+			checkImage[x][y][2] = (int)c;
+			checkImage_copy[x][y][0] = (int)c;
+			checkImage_copy[x][y][1] = (int)c;
+			checkImage_copy[x][y][2] = (int)c;
+
+		}
+	}
+	glDrawPixels(checkImageWidth, checkImageHeight, GL_RGB, GL_UNSIGNED_BYTE, checkImage);
+	glFlush();
+}
+void dilate()
+{
+	int sum = 0, pixels = 0;
+	for (int x = 0; x < checkImageHeight; x++)
+	{
+		for (int y = 0; y < checkImageWidth; y++)
+		{
+			sum += checkImage[x][y][0];
+			sum += checkImage[x][y][1];
+			sum += checkImage[x][y][2];
+			pixels += 3;
+		}
+	}
+	printf("Sum: %d\n", sum);
+	printf("Total Pixels: %d\n", pixels);
+	int avg;
+	avg = (int)sum/pixels;
+	int new;
+	printf("Avg. grayscale value: %d\n", avg);
+	for (int x = 0; x < checkImageHeight; x++)
+	{
+		for (int y = 0; y < checkImageWidth; y++)
+		{
+			//channel 0
+			if (checkImage[x][y][0] > avg)
 			{
-				checkImage[x+1][y][0] += (GLubyte) 1;
+				new = checkImage[x][y][0] + 3;
+				if (new > 255)
+				{
+					checkImage[x][y][0] = 255;
+				}
+				else
+				{
+					checkImage[x][y][0] += 3;
+				}
+				if (x+1 < checkImageHeight)
+				{
+					new = checkImage[x+1][y][0] + 1;
+					if (new > 255)
+					{
+						checkImage[x+1][y][0] = 255;
+					}
+					else
+					{
+						checkImage[x+1][y][0] += 1;
+					}
+				}
+				if (x-1 > 0){
+					new = checkImage[x-1][y][0] + 1;
+					if (new > 255)
+					{
+						checkImage[x-1][y][0] = 255;
+					}
+					else
+					{
+						checkImage[x-1][y][0] + 1;
+					}
+				}
+				if (y+1 < checkImageWidth)
+				{
+					new = checkImage[x][y+1][0] + 1;
+					if (new > 255)
+					{
+						checkImage[x][y+1][0] = 255;
+					}
+					else
+					{
+						checkImage[x][y+1][0] += 1;
+					}
+				}
+				if (y-1 > 0)
+				{
+					new = checkImage[x][y-1][0] + 1;
+					if (new > 255)
+					{
+						checkImage[x][y-1][0] = 255;
+					}
+					else
+					{
+						checkImage[x][y-1][0] += 1;
+					}
+				}
 			}
-			if (x-1 > 0){
-				checkImage[x-1][y][0] += (GLubyte) 1;
-			}
-			if (y+1 < checkImageWidth)
+			//channel 1
+			if (checkImage[x][y][1] > avg)
 			{
-				checkImage[x][y+1][0] += (GLubyte) 1;
+				new = checkImage[x][y][1] + 3;
+				if (new > 255)
+				{
+					checkImage[x][y][1] = 255;
+				}
+				else
+				{
+					checkImage[x][y][1] += 3;
+				}
+				if (x+1 < checkImageHeight)
+				{
+					new = checkImage[x+1][y][1] + 1;
+					if (new > 255)
+					{
+						checkImage[x+1][y][1] = 255;
+					}
+					else
+					{
+						checkImage[x+1][y][1] += 1;
+					}
+				}
+				if (x-1 > 0){
+					new = checkImage[x-1][y][1] + 1;
+					if (new > 255)
+					{
+						checkImage[x-1][y][1] = 255;
+					}
+					else
+					{
+						checkImage[x-1][y][1] + 1;
+					}
+				}
+				if (y+1 < checkImageWidth)
+				{
+					new = checkImage[x][y+1][1] + 1;
+					if (new > 255)
+					{
+						checkImage[x][y+1][1] = 255;
+					}
+					else
+					{
+						checkImage[x][y+1][1] += 1;
+					}
+				}
+				if (y-1 > 0)
+				{
+					new = checkImage[x][y-1][1] + 1;
+					if (new > 255)
+					{
+						checkImage[x][y-1][1] = 255;
+					}
+					else
+					{
+						checkImage[x][y-1][1] += 1;
+					}
+				}
 			}
-			if (y-1 > 0)
+			//channel 2
+			if (checkImage[x][y][2] > avg)
 			{
-				checkImage[x][y-1][0] += (GLubyte) 1;
+				new = checkImage[x][y][2] + 3;
+				if (new > 255)
+				{
+					checkImage[x][y][2] = 255;
+				}
+				else
+				{
+					checkImage[x][y][2] += 3;
+				}
+				if (x+1 < checkImageHeight)
+				{
+					new = checkImage[x+1][y][2] + 1;
+					if (new > 255)
+					{
+						checkImage[x+1][y][2] = 255;
+					}
+					else
+					{
+						checkImage[x+1][y][2] += 1;
+					}
+				}
+				if (x-1 > 0){
+					new = checkImage[x-1][y][2] + 1;
+					if (new > 255)
+					{
+						checkImage[x-1][y][2] = 255;
+					}
+					else
+					{
+						checkImage[x-1][y][2] + 1;
+					}
+				}
+				if (y+1 < checkImageWidth)
+				{
+					new = checkImage[x][y+1][2] + 1;
+					if (new > 255)
+					{
+						checkImage[x][y+1][2] = 255;
+					}
+					else
+					{
+						checkImage[x][y+1][2] += 1;
+					}
+				}
+				if (y-1 > 0)
+				{
+					new = checkImage[x][y-1][2] + 1;
+					if (new > 255)
+					{
+						checkImage[x][y-1][2] = 255;
+					}
+					else
+					{
+						checkImage[x][y-1][2] += 1;
+					}
+				}
 			}
-			
-			checkImage[x][y][1] += (GLubyte) 3;
-			if (x+1 < checkImageHeight)
+		}
+	}
+	glDrawPixels(checkImageWidth, checkImageHeight, GL_RGB, GL_UNSIGNED_BYTE, checkImage);
+	glFlush();
+}
+
+void erode()
+{
+	int sum = 0, pixels = 0;
+	for (int x = 0; x < checkImageHeight; x++)
+	{
+		for (int y = 0; y < checkImageWidth; y++)
+		{
+			sum += checkImage[x][y][0];
+			sum += checkImage[x][y][1];
+			sum += checkImage[x][y][2];
+			pixels += 3;
+		}
+	}
+	printf("Sum: %d\n", sum);
+	printf("Total Pixels: %d\n", pixels);
+	int avg;
+	avg = (int)sum/pixels;
+	printf("Avg. grayscale value: %d\n", avg);
+	int new2;
+	
+	for (int x = 0; x < checkImageHeight; x++)
+	{
+		for (int y = 0; y < checkImageWidth; y++)
+		{
+			if (checkImage[x][y][0] > avg)
 			{
-				checkImage[x+1][y][1] += (GLubyte) 1;
+				new2 = checkImage_copy[x][y][0] - 3;
+				if (new2 < 0)
+				{
+					checkImage_copy[x][y][0] = 0;
+				}
+				else
+				{
+					checkImage_copy[x][y][0] -= 3;
+				}
+				if (x+1 < checkImageHeight)
+				{
+					new2 = checkImage_copy[x+1][y][0] - 1;
+					if (new2 < 0)
+					{
+						checkImage_copy[x+1][y][0] = 0;
+					}
+					else
+					{
+						checkImage_copy[x+1][y][0] =- 1;
+					}
+				}
+				if (x-1 > 0){
+					new2 = checkImage_copy[x-1][y][0] - 1;
+					if (new2 < 0)
+					{
+						checkImage_copy[x-1][y][0] = 0;
+					}
+					else
+					{
+						checkImage_copy[x-1][y][0] -= 1;
+					}
+				}
+				if (y+1 < checkImageWidth)
+				{
+					new2 = checkImage_copy[x][y+1][0] - 1;
+					if (new2 < 0)
+					{
+						checkImage_copy[x][y+1][0] = 0;
+					}
+					else
+					{
+						checkImage_copy[x][y+1][0] -= 1;
+					}
+				}
+				if (y-1 > 0)
+				{
+					new2 = checkImage_copy[x][y-1][0] - 1;
+					if (new2 < 0)
+					{
+						checkImage_copy[x][y-1][0] = 0;
+					}
+					else
+					{
+						checkImage_copy[x][y-1][0] -= 1;
+					}
+				}
 			}
-			if (x-1 > 0){
-				checkImage[x-1][y][1] += (GLubyte) 1;
-			}
-			if (y+1 < checkImageWidth)
+			//channel 1
+			if (checkImage[x][y][1] > avg)
 			{
-				checkImage[x][y+1][1] += (GLubyte) 1;
+					
+				new2 = checkImage_copy[x][y][1] - 3;
+				if (new2 < 0)
+				{
+					checkImage_copy[x][y][1] = 0;
+				}
+				else
+				{
+					checkImage_copy[x][y][1] -= 3;
+				}
+				if (x+1 < checkImageHeight)
+				{
+					new2 = checkImage_copy[x+1][y][1] - 1;
+					if (new2 < 0)
+					{
+						checkImage_copy[x+1][y][1] = 0;
+					}
+					else
+					{
+						checkImage_copy[x+1][y][1] =- 1;
+					}
+				}
+				if (x-1 > 0){
+					new2 = checkImage_copy[x-1][y][1] - 1;
+					if (new2 < 0)
+					{
+						checkImage_copy[x-1][y][1] = 0;
+					}
+					else
+					{
+						checkImage_copy[x-1][y][1] -= 1;
+					}
+				}
+				if (y+1 < checkImageWidth)
+				{
+					new2 = checkImage_copy[x][y+1][1] - 1;
+					if (new2 < 0)
+					{
+						checkImage_copy[x][y+1][1] = 0;
+					}
+					else
+					{
+						checkImage_copy[x][y+1][1] -= 1;
+					}
+				}
+				if (y-1 > 0)
+				{
+					new2 = checkImage_copy[x][y-1][1] - 1;
+					if (new2 < 0)
+					{
+						checkImage_copy[x][y-1][1] = 0;
+					}
+					else
+					{
+						checkImage_copy[x][y-1][1] -= 1;
+					}
+				}
 			}
-			if (y-1 > 0)
+			//channel 2
+			if (checkImage[x][y][2] > avg)
 			{
-				checkImage[x][y-1][1] += (GLubyte) 1;
+					
+				new2 = checkImage_copy[x][y][2] - 3;
+				if (new2 < 0)
+				{
+					checkImage_copy[x][y][2] = 0;
+				}
+				else
+				{
+					checkImage_copy[x][y][2] -= 3;
+				}
+				if (x+1 < checkImageHeight)
+				{
+					new2 = checkImage_copy[x+1][y][2] - 1;
+					if (new2 < 0)
+					{
+						checkImage_copy[x+1][y][2] = 0;
+					}
+					else
+					{
+						checkImage_copy[x+1][y][2] =- 1;
+					}
+				}
+				if (x-1 > 0){
+					new2 = checkImage_copy[x-1][y][2] - 1;
+					if (new2 < 0)
+					{
+						checkImage_copy[x-1][y][2] = 0;
+					}
+					else
+					{
+						checkImage_copy[x-1][y][2] -= 1;
+					}
+				}
+				if (y+1 < checkImageWidth)
+				{
+					new2 = checkImage_copy[x][y+1][2] - 1;
+					if (new2 < 0)
+					{
+						checkImage_copy[x][y+1][2] = 0;
+					}
+					else
+					{
+						checkImage_copy[x][y+1][2] -= 1;
+					}
+				}
+				if (y-1 > 0)
+				{
+					new2 = checkImage_copy[x][y-1][2] - 1;
+					if (new2 < 0)
+					{
+						checkImage_copy[x][y-1][2] = 0;
+					}
+					else
+					{
+						checkImage_copy[x][y-1][2] -= 1;
+					}
+				}
 			}
-			
-			checkImage[x][y][2] += (GLubyte) 3;
-			if (x+1 < checkImageHeight)
+		}
+	}
+	glDrawPixels(checkImageWidth, checkImageHeight, GL_RGB, GL_UNSIGNED_BYTE, checkImage_copy);
+	glFlush();
+}
+
+void edge_detect()
+{
+	int xyz;
+	dilate();
+	erode();
+	for (int x = 0; x < checkImageHeight; x++)
+	{
+		for (int y = 0; y < checkImageWidth; y++)
+		{
+			xyz = checkImage[x][y][0] - checkImage_copy[x][y][0];
+			if (xyz < 0)
 			{
-				checkImage[x+1][y][2] += (GLubyte) 1;
+				checkImage[x][y][0] = 255;
 			}
-			if (x-1 > 0){
-				checkImage[x-1][y][2] += (GLubyte) 1;
-			}
-			if (y+1 < checkImageWidth)
+			else
 			{
-				checkImage[x][y+1][2] += (GLubyte) 1;
+				checkImage[x][y][0] = 0;
 			}
-			if (y-1 > 0)
+			xyz = checkImage[x][y][1] - checkImage_copy[x][y][1];
+			if (xyz < 0)
 			{
-				checkImage[x][y-1][2] += (GLubyte) 1;
+				checkImage[x][y][1] = 255;
+			}
+			else
+			{
+				checkImage[x][y][1] = 0;
+			}
+			xyz = checkImage[x][y][2] - checkImage_copy[x][y][2];
+			if (xyz < 0)
+			{
+				checkImage[x][y][2] = 255;
+			}
+			else
+			{
+				checkImage[x][y][2] = 0;
 			}
 
 		}
@@ -117,6 +535,52 @@ void dilate()
 	glFlush();
 }
 
+void mask(){
+	int lgs, hgs;
+	printf("Enter lower grayscale value: ");
+	scanf("%3d", &lgs);
+	printf("Enter higher grayscale value: ");
+	scanf("%3d", &hgs);
+	for (int i = 0; i < checkImageHeight; i++) {
+		for (int j = 0; j < checkImageWidth; j++) {
+			if (checkImage[i][j][0] > lgs && checkImage[i][j][0] < hgs)
+			{
+				checkImage[i][j][0] = 0;
+				checkImage[i][j][1] = 0;
+				checkImage[i][j][2] = 0;
+			}
+		}
+	}
+	glDrawPixels(checkImageWidth, checkImageHeight, GL_RGB, GL_UNSIGNED_BYTE, checkImage);
+	glFlush();	
+}
+
+void reset()
+{
+   int i, j, c;
+   
+   int width2, height2, bpp2, row2;
+
+   uint8_t* rgb_image = stbi_load("./photos/test2.png", &width2, &height2, &bpp2, 3);
+   
+   printf("Resetting!... %d:%d\n", width2, height2);
+   
+   for (i = 0; i < height2; i++) {
+      for (j = 0; j < width2; j++) {
+         //c = ((((i&0x8)==0)^((j&0x8))==0))*255;
+         checkImage[i][j][0] = (GLubyte) rgb_image[((width2*height2-width2+j)-(i*width2))*3];
+         checkImage[i][j][1] = (GLubyte) rgb_image[((width2*height2-width2+j)-(i*width2))*3+1];
+         checkImage[i][j][2] = (GLubyte) rgb_image[((width2*height2-width2+j)-(i*width2))*3+2];
+         
+         checkImage_copy[i][j][0] = (GLubyte) rgb_image[((width2*height2-width2+j)-(i*width2))*3];
+         checkImage_copy[i][j][1] = (GLubyte) rgb_image[((width2*height2-width2+j)-(i*width2))*3+1];
+         checkImage_copy[i][j][2] = (GLubyte) rgb_image[((width2*height2-width2+j)-(i*width2))*3+2];
+      }
+   }
+   stbi_image_free(rgb_image);
+   glDrawPixels(checkImageWidth, checkImageHeight, GL_RGB, GL_UNSIGNED_BYTE, checkImage);
+   glFlush();	
+}
 
 void makeCheckImage(int width, int height,uint8_t* rgb_image)
 {
@@ -130,7 +594,10 @@ void makeCheckImage(int width, int height,uint8_t* rgb_image)
          checkImage[i][j][0] = (GLubyte) rgb_image[((width*height-width+j)-(i*width))*3];
          checkImage[i][j][1] = (GLubyte) rgb_image[((width*height-width+j)-(i*width))*3+1];
          checkImage[i][j][2] = (GLubyte) rgb_image[((width*height-width+j)-(i*width))*3+2];
-
+         
+         checkImage_copy[i][j][0] = (GLubyte) rgb_image[((width*height-width+j)-(i*width))*3];
+         checkImage_copy[i][j][1] = (GLubyte) rgb_image[((width*height-width+j)-(i*width))*3+1];
+         checkImage_copy[i][j][2] = (GLubyte) rgb_image[((width*height-width+j)-(i*width))*3+2];
       }
    }
 }
@@ -164,6 +631,21 @@ void keyboard(unsigned char key, int x, int y)
 		break;
       case 'd':
 		dilate();
+		break;
+      case 'e':
+		erode();
+		break;
+      case 'm':
+		mask();
+		break;
+      case 'r':
+		reset();
+		break;
+      case 'g':
+		make_grayscale();
+		break;
+      case 'p':
+		edge_detect();
 		break;
    }
 }
