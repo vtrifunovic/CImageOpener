9f43f8d8266d38a366a949238df57f5cf6409b7f
vtrifunovic <76224191+vtrifunovic@users.noreply.github.com>
2022-10-04 00:05:53 -0500

    Update rgbmask.c

--- src/binaryproc/rgbmask.c
+++ src/binaryproc/rgbmask.c
@@ -4,7 +4,7 @@
 #include "../render/render.h"
 
 void rgb_mask(K9_Image image, vec3 lower_bound, vec3 higher_bound){
-	// This function should not exceed memory allocated
+	// Similar to OpenCV's inRange() function
 	// test position of pixel to determine bound
 	long totalpixels = image.width * image.height * 3;
 	for (int c = 0; c < 3; c++){
