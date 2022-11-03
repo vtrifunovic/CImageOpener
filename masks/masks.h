5db742ec52ba07318a36bb9061c53cfa807babb2
vtrifunovic <76224191+vtrifunovic@users.noreply.github.com>
2022-11-03 09:16:34 -0500

    Delete masks directory

--- masks/masks.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../render/render.h"
-#include "../linmath.h"
-
-K9_Image rgb_mask(K9_Image image, vec3 lower_bound, vec3 higher_bound);
-void bitwiseAnd(K9_Image image, K9_Image mask);
-void bitwiseNot(K9_Image image, K9_Image mask);
-void grayscale_mask(K9_Image image, int lower_bound, int higher_bound);
