ce9b2ec91bf3ee16521a8467f3da05ede89e8479
vtrifunovic <76224191+vtrifunovic@users.noreply.github.com>
2022-11-03 09:16:26 -0500

    Delete conversions directory

--- conversions/conversions.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../render/render.h"
-#include "../linmath.h"
-
-K9_Image rgb_to_gray(K9_Image image);
-K9_Image rgb_to_hsv(K9_Image image);
-void invert(K9_Image image);
-K9_Image resize_img(K9_Image image, vec2 scale, char *type);
