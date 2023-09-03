46f5a28be1d527a82c47595479535eff39ea34f1
vtrifunovic <76224191+vtrifunovic@users.noreply.github.com>
2023-09-02 22:11:00 -0500

    forgot to update typename.h 💀

--- src/typename.h
+++ src/typename.h
@@ -8,3 +8,9 @@
 #define K9_IGNORE 0xFFFFFF
 #define K9_N4 0xA7
 #define K9_N8 0xA8
+#define K9_RGB2HSV 0xA9
+#define K9_RGB2GRAY 0xAA
+#define K9_INVERT 0xAB
+#define K9_HSV2RGB 0xAC
+#define K9_RGB2YUV 0xAD
+#define K9_YUV2RGB 0xAE
