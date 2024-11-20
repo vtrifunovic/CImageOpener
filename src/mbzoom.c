ce0a97cd5e1c35a42171f003f2c6bec1670318d5
vtrifunovic <76224191+vtrifunovic@users.noreply.github.com>
2024-11-19 18:39:17 -0600

    mbzoom now zooms

--- src/mbzoom.c
+++ src/mbzoom.c
@@ -1,15 +1,35 @@
-#include "render/render.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include "linmath.h"
+#include "global.h"
+#include "typename.h"
 #include "effects/effects.h"
 
-should_quit = false;
+int press = 0, held = 0;
 
-int main(){
-    K9_Image *img = load_image("test.JPG");
-    GLFWwindow *window = init_window(*img);
-    while(!should_quit){
-        mandelbrot_zoom(img, 1.0, 0, 0);
-        show_image(window, *img, false);
-        glfwPollEvents();
+int main(void){
+    struct timeval stop, start;
+    double scale = 1;
+    //double cenX = -1.253250005;
+    //double cenY = -0.344136949;
+    double cenX = -0.02532307;
+    double cenY = -0.65226512951;
+    K9_Image *img  = create_img(1200, 800, 1);
+    K9_Image *dimg = create_img(1200, 800, 3);
+    init_gpu(img);    
+    mbzoom(dimg, scale, cenX, cenY, true);
+    GLFWwindow *win = init_window(*img, "test");
+    gettimeofday(&start, NULL);
+    while (!handle_inputs(win)){
+        show_image(win, *dimg, false);
+        scale *= 1.05;
+        mbzoom(dimg, scale, cenX, cenY, true);
+        gettimeofday(&stop, NULL);
+        if (33333 > stop.tv_usec-start.tv_usec && stop.tv_usec-start.tv_usec > 0)
+            usleep(stop.tv_usec - start.tv_usec);
+        start = stop;
     }
-    return 0;
 }
